1a036a816660fbec358374c48a5f83c7956a7088
crndmrc <ceren_57_99@hotmail.com>
2022-03-17 14:44:20 +0300

    Add project files.

---
+++ ExampModelViewController/Controllers/DepartmanController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExampModelViewController.Models
+{
+    public class DepartmanController : Controller
+    {
+        //global 
+        InsanKaynaklariEntities _context = new InsanKaynaklariEntities();
+        // GET: Personel
+        public ActionResult Index()
+        {
+            List<Departman> dataDepartman = _context.Departman.ToList();
+
+            return View(dataDepartman);
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+
+            List<Departman> dataDepartman = _context.Departman.ToList();
+            ViewBag.dataDepartman = dataDepartman;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Departman _departman)
+        {
+            _context.Departman.Add(_departman);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+            //return View();
+        }
+        public ActionResult Delete(int id)
+        {
+            _context.Departman.Remove(_context.Departman.Find(id));
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ExampModelViewController/Controllers/PersonelController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExampModelViewController.Models
+{
+    public class PersonelController : Controller
+    {
+        //global 
+        InsanKaynaklariEntities _context = new InsanKaynaklariEntities();
+        // GET: Personel
+        public ActionResult Index()
+        {
+            List<Personel> dataPersonel = _context.Personel.ToList();
+
+            return View(dataPersonel);
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            List<Personel> dataPersonel = _context.Personel.ToList();
+            ViewBag.dataPersonel = dataPersonel;
+
+            List<Departman> dataDepartman = _context.Departman.ToList();
+            ViewBag.dataDepartman = dataDepartman;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Personel _personel)
+        {
+            _personel.Departman = _context.Departman.Find(_personel.departman_id);
+
+            _context.Personel.Add(_personel);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+            //return View();
+        }
+        public ActionResult Delete(int id)
+        {
+            _context.Personel.Remove(_context.Personel.Find(id));
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
